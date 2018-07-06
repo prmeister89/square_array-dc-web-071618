5e8cc4f864ad65781e508b35701a41887099f5c6
Peter Meister <prmeister89@gmail.com>
2018-07-06 03:10:50 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,11 @@
 def square_array(array)
+  new_array = []
   array.each do |element|
     element**2
+    element.push(new_array)
   end
-  return array!
+  return new_array
 end
+
 
 array = [1,2,3,4,5]
